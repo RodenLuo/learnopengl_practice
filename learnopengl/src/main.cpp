d54aaaf299efc3e5e2fbac36c7d15e6c8be50f66
Roden Luo <roden.luo@gmail.com>
2021-04-10 10:26:01 +0300

    two VAOs use the same VBO

--- learnopengl/src/main.cpp
+++ learnopengl/src/main.cpp
@@ -64,12 +64,12 @@ int main() {
 		0, 1, 3,
 	};
 
-	float vertices2[] = {
-		 0.5f,  0.5f, 0.0f,  // top right
-		 0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
-	};
+	//float vertices2[] = {
+	//	 0.5f,  0.5f, 0.0f,  // top right
+	//	 0.5f, -0.5f, 0.0f,  // bottom right
+	//	-0.5f, -0.5f, 0.0f,  // bottom left
+	//	-0.5f,  0.5f, 0.0f   // top left 
+	//};
 	unsigned int indices2[] = {
 		1, 2, 3
 	};
@@ -96,8 +96,8 @@ int main() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[1]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices2), indices2, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
