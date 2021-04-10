3fb1aaefd5413a54e75628bd71086f1928097312
Roden Luo <roden.luo@gmail.com>
2021-04-10 10:09:43 +0300

    two triangles in one VAOs array with two elements inside

--- learnopengl/src/main.cpp
+++ learnopengl/src/main.cpp
@@ -91,29 +91,16 @@ int main() {
 	glEnableVertexAttribArray(0);
 
 
-
-	
-
-	unsigned int VAO2;
-	glGenVertexArrays(1, &VAO2);
-	glBindVertexArray(VAO2);
-
-	unsigned int EBO2;
-	glGenBuffers(1, &EBO2);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
+	// second triangle
+	glBindVertexArray(VAOs[1]);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[1]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices2), indices2, GL_STATIC_DRAW);
 
-
-	unsigned int VBO2;
-	glGenBuffers(1, &VBO2);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindVertexArray(0);
 
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -195,13 +182,13 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
+		glBindVertexArray(VAOs[0]);
 
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glUseProgram(shaderProgram2);
-		glBindVertexArray(VAO2);
+		glBindVertexArray(VAOs[1]);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
