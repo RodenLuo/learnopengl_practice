7ad15328f75f95e724c0d217ef8936483f632d11
Roden Luo <roden.luo@gmail.com>
2021-04-10 13:43:41 +0300

    GLSL uniform

--- learnopengl/src/main.cpp
+++ learnopengl/src/main.cpp
@@ -17,11 +17,11 @@ const char* vertexShaderSource = "#version 330 core\n"
 
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
-	"in vec4 vertexColor;\n"
+	"uniform vec4 ourColor;\n"
 	"\n"
 	"void main()\n"
 	"{\n"
-	"    FragColor = vertexColor;\n"
+	"    FragColor = ourColor;\n"
 	"}\0";
 
 const char* fragmentShaderSource2 = "#version 330 core\n"
@@ -183,7 +183,12 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+
 		glUseProgram(shaderProgram);
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		glBindVertexArray(VAOs[0]);
 
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
